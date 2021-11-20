4ad2a90e8b30250d1d7be78e26dd0e0e22b610b8
Adrian Grimm <usmcamgrimm@gmail.com>
2021-11-20 12:13:39 -0500

    Create the players API v1 and update routes

---
+++ app/controllers/api/v1/players_controller.rb
@@ -0,0 +1,65 @@
+class Api::V1::PlayersController < ApplicationController
+  before_action :set_player, only: [:show, :edit, :update, :destroy]
+
+  # GET /players
+  # GET /players.json
+  def index
+    @players = Player.all.order(number: :asc)
+    render json: @players
+  end
+
+  # GET /players/1
+  # GET /players/1.json
+  def show
+    if @player
+      render json: @player
+    else
+      render json: @player.errors
+    end
+  end
+
+  # GET /players/new
+  def new
+    @player = Player.new
+  end
+
+  # GET /players/1/edit
+  def edit
+  end
+
+  # POST /players
+  # POST /players.json
+  def create
+    @player = Player.new(player_params)
+
+    if @beer.save
+      render json: @beer
+    else
+      render json: @beer.errors
+    end
+  end
+
+  # PATCH/PUT /players/1
+  # PATCH/PUT /players/1.json
+  def update
+  end
+
+  # DELETE /players/1
+  # DELETE /players/1.json
+  def destroy
+    @player.destroy
+
+    render json: { notice: 'Player has been removed from the roster.'}
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_player
+      @player = Player.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def player_params
+      params.permit(:name, :position, :number)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'players/index'
+      post 'players/create'
+      delete 'players/:id', to 'players#destroy'
+    end
+  end
+
   root 'players#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
