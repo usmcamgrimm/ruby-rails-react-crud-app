3413370b0db7ecf132195e937dbdfa478f3fe432
Adrian Grimm <usmcamgrimm@gmail.com>
2021-11-20 11:03:41 -0500

    Added the Player db and typed in the roster

---
+++ db/migrate/20211120153116_create_players.rb
@@ -0,0 +1,11 @@
+class CreatePlayers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.string :position
+      t.integer :number
+
+      t.timestamps
+    end
+  end
+end
